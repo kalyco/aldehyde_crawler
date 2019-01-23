25a277837431f28ab47c6ba5c23b78e96a9bf86a
Kayla Comalli <Kayla.comalli@gmail.com>
2019-01-23 18:28:53 -0500

    fix: close still

--- aldehydes.py
+++ aldehydes.py
@@ -1,6 +1,7 @@
 # Run me w $ python aldehydes.py
 from selenium import webdriver
 import os
+import time
 import urllib.request
 import logging
 from selenium.webdriver.common.by import By
@@ -10,35 +11,39 @@ from selenium.webdriver.support import expected_conditions as EC
 logging.basicConfig(filename='output.log',level=logging.DEBUG)
 
 wiki = 'https://en.m.wikipedia.org/wiki/Category%3AAldehydes'
-
-driver = webdriver.Firefox()
-wait = WebDriverWait(driver, 10)
 directory = '/aldehyde_images'
 
-def scrape(url):
-	try:
-		driver.get(url)
-		page = driver.find_element(By.ID, 'mw-pages')
-		categories = page.find_elements(By.CLASS_NAME, 'mw-category-group')
-		# for category in categories:
-		category = categories[0]
-		elems = category.find_elements_by_tag_name('a')
-		# for elem in elems:
-		elem = elems[0]
-		print(elem.text)
-		elem.click()
-		wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'thumbimage')))
-		img = driver.find_element(By.CLASS_NAME, 'thumbimage')
-		name = img.text
-		filename = '/something.png'
-		imgurl = img.get_attribute('src')
-		driver.get(imgurl)
-		wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'transparent')))
-		imagefile = os.getcwd() + directory + filename
-		print(imagefile)
-		urllib.request.urlretrieve(imgurl, imagefile)
-	finally:
-		driver.close
-		driver.quit
+def setUp():
+	options = webdriver.ChromeOptions()
+	options.add_argument('headless')
+	driver = webdriver.Chrome(chrome_options=options)
+	driver.set_window_size(1120, 550)
+	driver.get(wiki)
 
-scrape(wiki)
+	return driver
+
+def scrape():
+	driver = setUp()
+	wait = WebDriverWait(driver, 100)
+	page = driver.find_element(By.ID, 'mw-pages')
+	categories = page.find_elements(By.CLASS_NAME, 'mw-category-group')
+	for category in categories:
+		elems = category.find_elements_by_tag_name('a')
+		print(category.text)
+		for elem in elems:
+			name = elem.text
+			try:
+				elem.click()
+				wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'thumbimage')))
+				img = driver.find_element(By.CLASS_NAME, 'thumbimage')
+				filename = '/' + name + '.png'
+				imgurl = img.get_attribute('src')
+				driver.get(imgurl)
+				imagefile = os.getcwd() + directory + filename
+				print(imagefile)
+				urllib.request.urlretrieve(imgurl, imagefile)
+			finally:
+				driver.back()
+	driver.quit
+
+scrape()
