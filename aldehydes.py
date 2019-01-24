f6c259cf15128d124737e220ab6d3a1cee93518a
Kayla Comalli <Kayla.comalli@gmail.com>
2019-01-23 19:03:18 -0500

    fix: got it

--- aldehydes.py
+++ aldehydes.py
@@ -7,6 +7,7 @@ import logging
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 
 logging.basicConfig(filename='output.log',level=logging.DEBUG)
 
@@ -15,7 +16,7 @@ directory = '/aldehyde_images'
 
 def setUp():
 	options = webdriver.ChromeOptions()
-	options.add_argument('headless')
+	# options.add_argument('headless')
 	driver = webdriver.Chrome(chrome_options=options)
 	driver.set_window_size(1120, 550)
 	driver.get(wiki)
@@ -27,23 +28,24 @@ def scrape():
 	wait = WebDriverWait(driver, 100)
 	page = driver.find_element(By.ID, 'mw-pages')
 	categories = page.find_elements(By.CLASS_NAME, 'mw-category-group')
+	main_window = driver.current_window_handle
 	for category in categories:
 		elems = category.find_elements_by_tag_name('a')
-		print(category.text)
 		for elem in elems:
 			name = elem.text
-			try:
-				elem.click()
-				wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'thumbimage')))
-				img = driver.find_element(By.CLASS_NAME, 'thumbimage')
-				filename = '/' + name + '.png'
-				imgurl = img.get_attribute('src')
-				driver.get(imgurl)
-				imagefile = os.getcwd() + directory + filename
-				print(imagefile)
-				urllib.request.urlretrieve(imgurl, imagefile)
-			finally:
-				driver.back()
+			print(name)
+			elem.send_keys(Keys.COMMAND + Keys.RETURN)
+			driver.switch_to_window(driver.window_handles[-1])
+			img = driver.find_elements(By.TAG_NAME, 'img')[1]
+			filename = '/' + name + '.png'
+			imgurl = img.get_attribute('src')
+			print(imgurl)
+			driver.get(imgurl)
+			imagefile = os.getcwd() + directory + filename
+			print(imagefile)
+			urllib.request.urlretrieve(imgurl, imagefile)
+			driver.close()
+			driver.switch_to_window(main_window)
 	driver.quit
 
 scrape()
